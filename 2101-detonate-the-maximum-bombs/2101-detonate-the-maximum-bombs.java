d0c893144cb5c0d165c5628555447c5cd75089c7
jhoots40 <78231533+jhoots40@users.noreply.github.com>
2023-05-31 12:27:49 -0500

    Time: 427 ms (6.02%), Space: 43.9 MB (10.20%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -18,12 +18,12 @@ class Solution {
         int x = bombs[current][0], y = bombs[current][1], r = bombs[current][2];
         
         for(int i = 0; i < bombs.length; i++) {
-            if(i == current) continue;
+            if(i == current || exploded.containsKey(i)) continue;
             int x_i = bombs[i][0], y_i = bombs[i][1], r_i = bombs[i][2];
             double distance = Math.sqrt(Math.pow(x_i - x, 2) + Math.pow(y_i - y, 2));
             if(distance <= r) 
             {
-                if(exploded.containsKey(i)) continue;
+                //if(exploded.containsKey(i)) continue;
                 exploded.put(i, 0);
                 dfs(i, bombs, exploded);
             }
